88e6c86003dd4e918d48ccb1de4f63a85d4b52c9
Mickaël Pham <inbox@mickael.dev>
2020-05-07 21:47:33 -0700

    Add code coverage
    Complete the test suite to achieve 100% code coverage.

--- Gemfile
+++ Gemfile
@@ -8,8 +8,10 @@ gem 'zeitwerk'
 group :development, :test do
   gem 'pry-byebug'
   gem 'rubocop', require: false
+  gem 'simplecov'
 end
 
 group :test do
   gem 'rspec'
+  gem 'simplecov', require: false
 end
--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -12,14 +12,15 @@ RSpec.describe Invoice do
 
   let(:plays) do
     {
-      'miser' => { 'name' => 'The Miser', 'type' => 'comedy' }
+      'miser' => { 'name' => 'The Miser', 'type' => play_type }
     }
   end
 
-  describe 'statement' do
-    context 'without credits earned' do
-      let(:attendees) { 2 }
+  let(:play_type) { 'comedy' }
+  let(:attendees) { 2 }
 
+  describe 'statement' do
+    context 'with a small audience' do
       let(:want) do
         <<~STATEMENT
           Statement for Lean Startup
@@ -29,12 +30,12 @@ RSpec.describe Invoice do
         STATEMENT
       end
 
-      it 'generates an invoice statement with 0 credits' do
+      it 'generates an invoice statement with no credits earned' do
         expect(Invoice.statement(invoice, plays)).to eq(want)
       end
     end
 
-    context 'with credits earned' do
+    context 'with a large audience' do
       let(:attendees) { 200 }
 
       let(:want) do
@@ -46,8 +47,35 @@ RSpec.describe Invoice do
         STATEMENT
       end
 
-      it 'generates the invoice statement with 210 credits' do
+      it 'generates the invoice statement with credits earned' do
         expect(Invoice.statement(invoice, plays)).to eq(want)
+      end
+    end
+
+    context 'when the play is a tragedy with a large audience' do
+      let(:play_type) { 'tragedy' }
+      let(:attendees) { 300 }
+
+      let(:want) do
+        <<~STATEMENT
+          Statement for Lean Startup
+            The Miser: $3100.00 (#{attendees} seats)
+          Amount owned is $3100.00
+          You earned 270 credits
+        STATEMENT
+      end
+
+      it 'generates an invoice statement with a different pricing' do
+        expect(Invoice.statement(invoice, plays)).to eq(want)
+      end
+    end
+
+    context 'when the play is of an unknown type' do
+      let(:play_type) { 'sci-fi' }
+
+      it 'raises a runtime error' do
+        expect { Invoice.statement(invoice, plays) }.
+          to raise_error "unknown type: sci-fi"
       end
     end
   end
